f6b160a8bda53b8525ddc6966af7d5bc6935570b
himynameisriz <v.rizwanali@gmail.com>
2017-03-30 14:23:09 -0500

    Add Sum and Hamming's

--- LeetCode/LeetCode/Program.cs
+++ LeetCode/LeetCode/Program.cs
@@ -10,21 +10,76 @@ namespace LeetCode
         {
             Console.WriteLine("Hello World!");
             var sw = new Stopwatch();
+            
 
-            sw.Start();
-            var twoSum = TwoSum_InProgress(new int[] { 230, 863, 916, 585, 981, 404, 316, 785, 88, 12, 70, 435, 384, 778, 887, 755, 740, 337, 86, 92, 325, 422, 815, 650, 920, 125, 277, 336, 221, 847, 168, 23, 677, 61, 400, 136, 874, 363, 394, 199, 863, 997, 794, 587, 124, 321, 212, 957, 764, 173, 314, 422, 927, 783, 930, 282, 306, 506, 44, 926, 691, 568, 68, 730, 933, 737, 531, 180, 414, 751, 28, 546, 60, 371, 493, 370, 527, 387, 43, 541, 13, 457, 328, 227, 652, 365, 430, 803, 59, 858, 538, 427, 583, 368, 375, 173, 809, 896, 370, 789 }, 542);
-            sw.Stop();
-            Console.WriteLine($"Two sums O(log n) - Total time: {sw.ElapsedMilliseconds}ms; Answer = {twoSum[0]}, {twoSum[1]}");
+            // Still understanding
+            #region Hamming
+            //sw.Reset();
+            //var i = HammingDistance(1, 4);
+            #endregion
+
+
+            // Understand
+            #region AddSum
 
             sw.Reset();
+            AddSum(38);
+            var x = Congruence_AddSum(65536);
+            Console.WriteLine(x);
 
-            sw.Start();
-            var twoSum_On = TwoSum_Works(new int[] { 230,863,916,585,981,404,316,785,88,12,70,435,384,778,887,755,740,337,86,92,325,422,815,650,920,125,277,336,221,847,168,23,677,61,400,136,874,363,394,199,863,997,794,587,124,321,212,957,764,173,314,422,927,783,930,282,306,506,44,926,691,568,68,730,933,737,531,180,414,751,28,546,60,371,493,370,527,387,43,541,13,457,328,227,652,365,430,803,59,858,538,427,583,368,375,173,809,896,370,789 }, 542);
-            sw.Stop();
-            Console.WriteLine($"Two sums O(n) - Total time: {sw.ElapsedMilliseconds}ms;  Answer = {twoSum_On[0]}, {twoSum_On[1]}");
+            #endregion
 
+            #region Two sums
+            //sw.Reset();
 
+            //sw.Start();
+            //var twoSum = TwoSum_InProgress(new int[] { 230, 863, 916, 585, 981, 404, 316, 785, 88, 12, 70, 435, 384, 778, 887, 755, 740, 337, 86, 92, 325, 422, 815, 650, 920, 125, 277, 336, 221, 847, 168, 23, 677, 61, 400, 136, 874, 363, 394, 199, 863, 997, 794, 587, 124, 321, 212, 957, 764, 173, 314, 422, 927, 783, 930, 282, 306, 506, 44, 926, 691, 568, 68, 730, 933, 737, 531, 180, 414, 751, 28, 546, 60, 371, 493, 370, 527, 387, 43, 541, 13, 457, 328, 227, 652, 365, 430, 803, 59, 858, 538, 427, 583, 368, 375, 173, 809, 896, 370, 789 }, 542);
+            //sw.Stop();
+            //Console.WriteLine($"Two sums O(log n) - Total time: {sw.ElapsedMilliseconds}ms; Answer = {twoSum[0]}, {twoSum[1]}");
 
+            //sw.Reset();
+
+            //sw.Start();
+            //var twoSum_On = TwoSum_Works(new int[] { 230,863,916,585,981,404,316,785,88,12,70,435,384,778,887,755,740,337,86,92,325,422,815,650,920,125,277,336,221,847,168,23,677,61,400,136,874,363,394,199,863,997,794,587,124,321,212,957,764,173,314,422,927,783,930,282,306,506,44,926,691,568,68,730,933,737,531,180,414,751,28,546,60,371,493,370,527,387,43,541,13,457,328,227,652,365,430,803,59,858,538,427,583,368,375,173,809,896,370,789 }, 542);
+            //sw.Stop();
+            //Console.WriteLine($"Two sums O(n) - Total time: {sw.ElapsedMilliseconds}ms;  Answer = {twoSum_On[0]}, {twoSum_On[1]}"); 
+            #endregion
+        }
+
+        #region AddSum
+        public static int AddSum(int num)
+        {
+            var x = 0;
+            var y = 0;
+            if (num < 10) return num;
+            while ((num / 10) > 0)
+            {
+                x = num / 10;
+                y = num % 10;
+                if (x + y < 10) return x + y;
+                else num = x + y;
+            }
+
+            return x + y;
+        }
+        public static int Congruence_AddSum(int num)
+        {
+            return 1 + (num - 1) % 9;
+        } 
+        #endregion
+
+        public static int HammingDistance(int x, int y)
+        {
+            int count = 0;
+            // bitwise xor
+            int diff = x ^ y;
+            while (diff != 0)
+            {
+                count++;
+                // int-32 
+                diff = diff & (diff - 1);
+            }
+            return count;
         }
 
 
@@ -43,7 +98,7 @@ namespace LeetCode
             var result = new int[] { 0, 0 };
             var i = 0;
             var j = nums.Length - 1;
-            
+
             while (i < j)
             {
                 var sum = sortedNums[i] + sortedNums[j];
@@ -83,7 +138,7 @@ namespace LeetCode
                     return result;
                 }
                 else
-                    if(!dict.ContainsKey(target-nums[i]))
+                    if (!dict.ContainsKey(target - nums[i]))
                     dict.Add(target - nums[i], i);
             }
             return result;
